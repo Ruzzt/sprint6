f0bcc70939a3008b9d13c2261a62ec07d53832cc
Tatiana Ruzavina <tat.ruzavina77@gmail.com>
2025-07-13 15:40:10 +0300

    first commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,61 @@
 package handlers
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func IndexFunc(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, "Ошибка парсинга формы", http.StatusInternalServerError)
+		return
+	}
+	file, header, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, "Ошибка получения файла", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	response, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "Ошибка чтения файла", http.StatusInternalServerError)
+		return
+	}
+
+	convert, err := service.Convert(response)
+	if err != nil {
+		http.Error(w, "Ошибка конвертации файла", http.StatusInternalServerError)
+		return
+	}
+
+	timestamp := time.Now().UTC().Format("20250713T150405Z")
+	ext := filepath.Ext(header.Filename)
+	localFileName := timestamp + ext
+
+	localFile, err := os.Create(localFileName)
+	if err != nil {
+		http.Error(w, "Ошибка создания файла", http.StatusInternalServerError)
+		return
+	}
+	defer localFile.Close()
+
+	_, err = localFile.WriteString(convert)
+	if err != nil {
+		http.Error(w, "Ошибка записи в файл", http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(convert))
+
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,78 @@
 package service
+
+import (
+	"errors"
+	"strings"
+)
+
+var textToMorse = map[rune]string{
+	'A': ".-", 'B': "-...", 'C': "-.-.", 'D': "-..",
+	'E': ".", 'F': "..-.", 'G': "--.", 'H': "....",
+	'I': "..", 'J': ".---", 'K': "-.-", 'L': ".-..",
+	'M': "--", 'N': "-.", 'O': "---", 'P': ".--.",
+	'Q': "--.-", 'R': ".-.", 'S': "...", 'T': "-",
+	'U': "..-", 'V': "...-", 'W': ".--", 'X': "-..-",
+	'Y': "-.--", 'Z': "--..",
+	'0': "-----", '1': ".----", '2': "..---", '3': "...--",
+	'4': "....-", '5': ".....", '6': "-....", '7': "--...",
+	'8': "---..", '9': "----.",
+	' ': "/",
+}
+
+var morseToText map[string]rune
+
+func init() {
+	morseToText = make(map[string]rune)
+	for k, v := range textToMorse {
+		morseToText[v] = k
+	}
+}
+
+func Convert(input []byte) (string, error) {
+	str := strings.TrimSpace(string(input))
+	if str == "" {
+		return "", errors.New("пустая строка")
+	}
+
+	isMorse := true
+	for _, c := range str {
+		if c != '.' && c != '-' && c != ' ' && c != '/' {
+			isMorse = false
+			break
+		}
+	}
+
+	if isMorse {
+
+		words := strings.Split(str, " / ")
+		var result []string
+		for _, word := range words {
+			letters := strings.Split(word, " ")
+			var decodedWord strings.Builder
+			for _, l := range letters {
+				if l == "" {
+					continue
+				}
+				if ch, ok := morseToText[l]; ok {
+					decodedWord.WriteRune(ch)
+				} else {
+					return "", errors.New("некорректный код Морзе: " + l)
+				}
+			}
+			result = append(result, decodedWord.String())
+		}
+		return strings.Join(result, " "), nil
+	} else {
+
+		var morse []string
+		upper := strings.ToUpper(str)
+		for _, c := range upper {
+			if code, ok := textToMorse[c]; ok {
+				morse = append(morse, code)
+			} else {
+				return "", errors.New("недопустимый символ в тексте: " + string(c))
+			}
+		}
+		return strings.Join(morse, " "), nil
+	}
+}
